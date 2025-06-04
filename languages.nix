e8a3a1c2981a48b44b209f9a68ac36cdb64e9bc7
will <will@256k1.dev>
2025-06-04 21:48:39 +0100

    split flake a bit

---
+++ languages.nix
@@ -0,0 +1,27 @@
+{ pkgs }: {
+  clang = {
+    enable = true;
+    lsp.enable = true;
+    treesitter.enable = true;
+  };
+  nix = {
+    enable = true;
+    format.enable = true;
+    lsp.enable = true;
+    lsp.package = pkgs.nil;
+    treesitter.enable = true;
+  };
+  python = {
+    enable = true;
+    lsp.enable = true;
+    lsp.package = pkgs.basedpyright;
+    treesitter.enable = true;
+    format.enable = true;
+    format.type = "ruff";
+  };
+  rust = {
+    enable = true;
+    lsp.enable = true;
+    treesitter.enable = true;
+  };
+}
