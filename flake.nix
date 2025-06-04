bd440ae2901dcdb7eb87c6688279e65d4a07d1d6
will <will@256k1.dev>
2025-06-04 21:12:17 +0100

    add more languages

--- flake.nix
+++ flake.nix
@@ -13,6 +13,33 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
+      languages = {
+        clang = {
+          enable = true;
+          lsp.enable = true;
+          treesitter.enable = true;
+        };
+        nix = {
+          enable = true;
+          format.enable = true;
+          lsp.enable = true;
+          lsp.package = pkgs.nil;
+          treesitter.enable = true;
+        };
+        python = {
+          enable = true;
+          lsp.enable = true;
+          lsp.package = pkgs.basedpyright;
+          treesitter.enable = true;
+          format.enable = true;
+          format.type = "ruff";
+        };
+        rust = {
+          enable = true;
+          lsp.enable = true;
+          treesitter.enable = true;
+        };
+      };
     in {
       packages.default =
         (nvf.lib.neovimConfiguration {
@@ -20,9 +47,10 @@
           modules = [
             {
               config.vim = {
+                inherit languages;
+                lsp.enable = true;
                 theme.enable = true;
                 treesitter.enable = true;
-                lsp.enable = true;
               };
             }
           ];
