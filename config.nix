e3e66a7b87a8c8e0b5c58c79269ade8a8e621fb3
will <will@256k1.dev>
2025-06-04 22:29:32 +0100

    use correct options and globals

--- config.nix
+++ config.nix
@@ -1,7 +1,6 @@
 {pkgs}: {
   config.vim =
     {
-      languages = import ./languages.nix {inherit pkgs;};
       autocomplete.blink-cmp = {
         enable = true;
         friendly-snippets.enable = true;
@@ -19,9 +18,72 @@
       };
       enableLuaLoader = true;
       git.gitsigns.enable = true;
+      languages = import ./languages.nix {inherit pkgs;};
       lsp.enable = true;
-      luaConfigRC.custom-options = builtins.readFile ./options.lua;
-      statusline.lualine.enable = true;
+
+      # Global variables
+      globals = {
+        mapleader = " ";
+        maplocalleader = " ";
+      };
+
+      # Vim options
+      options = {
+        hlsearch = false;
+        number = true;
+        relativenumber = true;
+        signcolumn = "yes";
+        wrap = false;
+        splitbelow = true;
+        splitright = true;
+        expandtab = true;
+        shiftwidth = 4;
+        tabstop = 4;
+        softtabstop = 4;
+        shell = "bash";
+        mouse = "a";
+        breakindent = true;
+        undofile = true;
+        ignorecase = true;
+        smartcase = true;
+        updatetime = 250;
+        conceallevel = 0;
+        cursorline = true;
+        clipboard = "unnamedplus";
+        listchars = "trail:~,tab:>-,nbsp:␣";
+        list = true;
+        laststatus = 3;
+        showmode = false;
+        linebreak = true;
+        breakindentopt = "list:-1";
+        backup = true;
+      };
+
+      # Key mappings
+      keymaps = [
+        {
+          key = "<Space>";
+          mode = ["n" "v"];
+          action = "<Nop>";
+          desc = "Disable space key";
+        }
+      ];
+
+      # Custom lua configuration using DAG
+      luaConfigRC = {
+        # Directory setup - needs to be done early
+        directory-setup = ''
+          -- Set directories for swap, backup, and undo files
+          local data_dir = vim.fn.stdpath("data")
+          vim.opt.directory = data_dir .. "/swap//"
+          vim.opt.backupdir = data_dir .. "/backup//"
+          vim.opt.undodir = data_dir .. "/undo//"
+        '';
+      };
+      statusline.lualine = {
+        enable = true;
+        theme = "auto";
+      };
       theme.enable = true;
       treesitter.enable = true;
       visuals.fidget-nvim.enable = true;
