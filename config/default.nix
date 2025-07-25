f9cba29116a93dcbd3ac469669f838cc64ce979c
will <will@256k1.dev>
2025-07-25 15:40:30 +0100

    lastplace: don't apply to git commits

--- config/default.nix
+++ config/default.nix
@@ -147,9 +147,11 @@
           callback = lib.generators.mkLuaInline ''
             function(args)
               local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) <= vim.fn.line('$')
-              local not_commit = vim.b[args.buf].filetype ~= 'commit'
+              local ignore_filetypes = { "gitcommit", "gitrebase", "svn", "hgcommit" }
+              local filetype = vim.bo[args.buf].filetype
+              local should_ignore = vim.tbl_contains(ignore_filetypes, filetype)
 
-              if valid_line and not_commit then
+              if valid_line and not should_ignore then
                 vim.cmd([[normal! g`"]])
               end
             end
